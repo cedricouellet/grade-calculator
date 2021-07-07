6c3e5944969add50bb8f143aa2e853fddd0ea117
Cedric Ouellet <1925921@cstjean.qc.ca>
2021-07-06 23:14:32 -0400

    slight design changes

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 /*
  * Author : Cedric-Antoine Ouellet
- * Date   : 2021/01/28
  */
 
-const btnCalculate = document.getElementById('btn-calculate');
-btnCalculate.addEventListener('click', () => {
-    const current = document.getElementById('current-grade').value;
-    const finalWeight = document.getElementById('exam-weight').value;
-    const goal = document.getElementById('wanted-grade').value;
-    const required = calculateRequired(parseInt(current), parseInt(finalWeight), parseInt(goal)).toFixed(1);
-    if (required !== "NaN") {
-        document.getElementById('required').innerText = 'Required: ' + required + '%';
-    }
+const btnCalculate = document.getElementById("btn-calculate");
+btnCalculate.addEventListener("click", () => {
+  const current = document.getElementById("current-grade").value;
+  const finalWeight = document.getElementById("exam-weight").value;
+  const goal = document.getElementById("wanted-grade").value;
+  const required = calculateRequired(
+    parseInt(current),
+    parseInt(finalWeight),
+    parseInt(goal)
+  ).toFixed(1);
+  if (required !== "NaN") {
+    document.getElementById("required").innerText =
+      "Required: " + required + "%";
+  }
 });
 
 function calculateRequired(current, finalWeight, goal) {
-    finalWeight /= 100;
-    return (goal - ((1 - finalWeight) * current)) / finalWeight;
+  finalWeight /= 100;
+  return (goal - (1 - finalWeight) * current) / finalWeight;
 }
